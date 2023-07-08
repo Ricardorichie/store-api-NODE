1a02ca39d03f94d8f0f81a60c7a2162d62fe0aa0
Richard Omoyele <109069693+Richiee-D@users.noreply.github.com>
2023-07-08 12:26:31 +0100

    product updated

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,39 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  throw new Error("testing async errors");
+  const products = await Product.find({ price: { $gt: 30 } });
+  // throw new Error("testing async errors");
   res.status(200).json({ msg: "products testing route" });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  // if (company) {
+  //   queryObject.company = company;
+  // }
+  // if (name) {
+  //   queryObject.name = { $regex: name, $options: "i" };
+  // }
+  // if (numericFilters) {
+  //   const operatorMap = {
+  //     ">": "$gt",
+  //     ">=": "$gte",
+  //     "=": "$eq",
+  //     "<": "$lt",
+  //     "<=": "$lte",
+  //   };
+  //   const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+  //   let filters = numericFilters.replace(
+  //     regEx,
+  //     (match) => `-${operatorMap[match]}-`
+  //   );
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
