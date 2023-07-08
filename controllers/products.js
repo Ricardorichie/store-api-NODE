6fa6b934b93e312b46679c0562cfc754adea5bc9
Richard Omoyele <109069693+Richiee-D@users.noreply.github.com>
2023-07-08 19:23:29 +0100

    filter on number fields

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,8 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ price: { $gt: 30 } });
-  // throw new Error("testing async errors");
-  res.status(200).json({ msg: "products testing route" });
+  const products = await Product.find({}).sort("price");
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
@@ -12,27 +11,55 @@ const getAllProducts = async (req, res) => {
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
-  // if (company) {
-  //   queryObject.company = company;
-  // }
-  // if (name) {
-  //   queryObject.name = { $regex: name, $options: "i" };
-  // }
-  // if (numericFilters) {
-  //   const operatorMap = {
-  //     ">": "$gt",
-  //     ">=": "$gte",
-  //     "=": "$eq",
-  //     "<": "$lt",
-  //     "<=": "$lte",
-  //   };
-  //   const regEx = /\b(<|>|>=|=|<|<=)\b/g;
-  //   let filters = numericFilters.replace(
-  //     regEx,
-  //     (match) => `-${operatorMap[match]}-`
-  //   );
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  //numeric filters for numeric fields (e.g price,rating) from greater than or equal to, less than or equal to, and equal to
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
+  }
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  //sort products
+  if (sort) {
+    const splitList = sort.split(",").join(" ");
+    result = result.sort(splitList);
+  } else {
+    result = result.sort("createdAt");
+  }
+  //Select fields to return
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+  //pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
